621aefa3dac465147eff86fab215533c5a2425fb
Selvin21-rng <selvinrajan54@gmail.com>
2025-01-23 14:23:11 +0530

    Add files via upload

---
+++ 2.)opposite sign sum.java
@@ -0,0 +1,31 @@
+2.)opposite sign sum
+
+import java.util.*;
+public class Main {
+	int ans;
+	void sum(int s) {
+	    int ans=-(s+1);
+		System.out.println(ans);
+
+	}
+
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+	System.out.println("enter num");
+		int s=sc.nextInt();
+		
+		Main t=new Main();
+		t.sum(s);
+
+
+
+
+
+
+
+
+
+	}
+}
+
